e133a21c9415c7ef4dec40129cc35a9c8a9d1120
董兴龙 <ragnar@dongxinglongdeMacBook-Pro.local>
2019-05-17 09:55:43 +0800

    基本功能实现

--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -6,18 +6,29 @@ Page({
   data: {
     input:'',
     leftCount: 0,
+    filter:1,
     allCompleted: false,
     todos:[
       { value: '晨跑打卡 滴～～', completed: false},
-      { value: '按时上班💼', completed: false},
+      { value: '按时上班💼', completed: true},
       { value: '谁个午觉好吧👌', completed: false},
       {  value: '下班回家溜溜溜', completed: false}
-    ]
+    ],
+    status: '1',
+    curLists:[],
+    showTodo:[]
   },
 
+  /**
+     * 生命周期函数--监听页面加载
+     */
+  onLoad: function (options) {
+    this.setData({
+      showTodo: this.data.todos
+    })
+  },
   save: function () {
     wx.setStorageSync('todo_list', this.data.todos)
-    wx.setStorageSync('todo_logs', this.data.logs)
   },
 
   inputChangeHandle: function (e) {
@@ -30,10 +41,10 @@ Page({
     todos.push({ value: this.data.input, completed: false })
     this.setData({
       input: '',
-      todos: todos,
+      showTodo: todos,
       leftCount: this.data.leftCount + 1,
     })
-    this.save()
+    this.save();
   },
   
   toggleTodoHandle: function (e) {
@@ -57,12 +68,48 @@ Page({
     })
     this.save()
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+
+  toggleAllHandle: function (e) {
+    this.data.allCompleted = !this.data.allCompleted
+    var todos = this.data.todos
+    for (var i = todos.length - 1; i >= 0; i--) {
+      todos[i].completed = this.data.allCompleted
+    }
+    this.setData({
+      todos: todos,
+      leftCount: this.data.allCompleted ? 0 : todos.length,
+    })
+    this.save()
   },
+
+  clearCompletedHandle: function (e) {
+    var todos = this.data.todos
+    var remains = []
+    for (var i = 0; i < todos.length; i++) {
+      todos[i].completed || remains.push(todos[i])
+    }
+
+    this.setData({ todos: remains })
+    this.save()
+  },
+  showStatus: function (e) {
+    var st = e.currentTarget.dataset.status
+    if (this.data.status === st) return
+    if (st === '') {
+      this.setData({
+        status: st,
+        curLists: this.data.lists
+      })
+      return
+    }
+    this.setData({
+      status: st,
+    })
+  },
+
+
+ 
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
